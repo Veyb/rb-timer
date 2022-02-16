c6d936de12ad96829431feddc056bfc78f0e8ec0
Oleg Tsyro <veymovie@gmail.com>
2022-02-16 12:39:08 +0300

    FRONT: socket WIP;

---
+++ frontend/lib/web-sockets.ts
@@ -0,0 +1,12 @@
+// import io from 'socket.io-client';
+
+import { API_URL } from './api';
+// let STRAPI_ENDPOINT;
+
+// if (process.env.NODE_ENV !== 'production') {
+//     STRAPI_ENDPOINT = 'http://localhost:1337';
+// } else {
+//     STRAPI_ENDPOINT = process.env.REACT_APP_SERVER_URL
+// }
+
+// export const socket = io(API_URL);
